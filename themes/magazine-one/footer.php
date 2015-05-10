b4ccbb40fe5de9d4c13b625cd24676545ae079d9
Diego Mendoza <dmendoza@inflexionsoftware.com>
2015-05-09 23:50:25 -0500

    Se borro el tema que no se estaba utilizando

--- themes/magazine-one/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-<footer class="mh-footer">
-	<div class="wrapper-inner">
-		<p class="copyright">Este sitio fue personalizado por Kevin y Diego Inc</p>
-	</div>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
--- themes/magazine-one/sidebar.php
+++
@@ -1,8 +0,0 @@
-<aside class="mh-sidebar">
-	<div id="sponsor-two">
-		<a href="#">
-			<?php echo '<img src="' . get_template_directory_uri() . '/images/sponsor2.png' . '" alt="No Picture" />'; ?>
-		</a>
-	</div>
-	<?php dynamic_sidebar('sidebar'); ?>
-</aside>
