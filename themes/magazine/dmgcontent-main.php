8be5336101a34f2b30d12e675885bb92a8eb2994
Diego Mendoza <dmendoza@inflexionsoftware.com>
2015-05-12 18:23:52 -0500

    Las categorías que se usan en el home se obtienen de la BD

--- themes/magazine/dmgcontent-main.php
+++ themes/magazine/dmgcontent-main.php
@@ -1,11 +1,17 @@
+<?php 
+	$cat1 = get_option( "home_cat1", "default" );
+	$cat2 = get_option( "home_cat2", "default" );
+	$cat3 = get_option( "home_cat3", "default" );
+	$cat4 = get_option( "home_cat4", "default" );
+?>
 <div>
 	<div class="clearfix">
 		<div class="category-style">
-			<?php query_posts('category_name=Jilotepec&showposts=3');?>
+			<?php query_posts('category_name='.$cat1.'&showposts=3');?>
 			 <?php get_template_part('dmgcontent','categories'); ?>
 		</div>
 		<div class="category-style">
-			<?php query_posts('category_name=Aculco&showposts=3');?>
+			<?php query_posts('category_name='.$cat2.'&showposts=3');?>
 			 <?php get_template_part('dmgcontent','categories'); ?>
 		</div>
 	</div>
@@ -15,11 +21,11 @@
 	</div>
 	<div class="clearfix">
 		<div class="category-style">
-			<?php query_posts('category_name=Soyaniquilpan&showposts=3');?>
+			<?php query_posts('category_name='.$cat3.'&showposts=3');?>
 			 <?php get_template_part('dmgcontent','categories'); ?>
 		</div>
 		<div class="category-style">
-			<?php query_posts('category_name=Chapa de mota&showposts=3');?>
+			<?php query_posts('category_name='.$cat4.'&showposts=3');?>
 			 <?php get_template_part('dmgcontent','categories'); ?>
 		</div>
 	</div>
