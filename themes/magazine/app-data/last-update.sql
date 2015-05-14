9a48efde734421af925e3e3630f5b9d84abeec9b
Diego Mendoza <dmendoza@inflexionsoftware.com>
2015-05-14 00:21:23 -0500

    Se agrego el footer con el menu dinamico. Mejoras en look and feel

---
+++ themes/magazine/app-data/last-update.sql
@@ -0,0 +1,4 @@
+INSERT INTO wp_options(option_name, option_value, autoload) VALUES('home_cat1','jilotepec', 'yes');
+INSERT INTO wp_options(option_name, option_value, autoload) VALUES('home_cat2','aculco', 'yes');
+INSERT INTO wp_options(option_name, option_value, autoload) VALUES('home_cat3','jilotepec', 'yes');
+INSERT INTO wp_options(option_name, option_value, autoload) VALUES('home_cat4','aculco', 'yes');
--- themes/magazine/footer.php
+++ themes/magazine/footer.php
@@ -1,87 +1,13 @@
 </div>
 <footer class="mh-footer">
-	<div id="footer" class="wrapper-inner">
-		<div class="content">
-			<div class="float">
-				<!--
-				<a href="http://jilo-magazine.esy.es/" >
-					<img src='wp-content/themes/magazine/images/quehayblack.JPG' alt='Imagen 1' />
-				</a> 
-				-->
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/en-la-torre/">
-								EN LA TORRE
-							</a>
-						</h5>
-					</div>
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/policiales/" class="">
-								POLICIALES
-							</a>
-						</h5>
-					</div>
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/cultura/" class="">
-								POLITICA
-							</a>
-						</h5>
-					</div>
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/cultura-2/" class="">
-								CULTURA
-							</a>
-						</h5>
-					</div>
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/deportes/" class="">
-								DEPORTES
-							</a>
-						</h5>
-					</div>
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/internacional/" class="">
-								INTERNACIONAL
-							</a>
-						</h5>
-					</div>
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/avisos-oportunos/" class="">
-								AV. OPORTUNOS
-							</a>
-						</h5>
-					</div>
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/educacion/" class="">
-								EDUCACIÓN
-							</a>
-						</h5>
-					</div>
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/social/" class="">
-								NACIONAL
-							</a>
-						</h5>
-					</div>
-					<div class="category-footer">
-						<h5 class="category-text-footer">
-							<a href="http://jilo-magazine.esy.es/category/nacionales/" class="">
-								SOCIAL
-							</a>
-						</h5>
-					</div>
-			</div>
-		</div>
+	<div>
+		<?php wp_nav_menu( array( 'menu' => 'General', 
+				'theme_location' => 'secondary',
+				'menu_id'      => 'footer-menu',
+				'before' => '<div class="footer-tab">',
+				'after' => '</div>' ) ); ?>
+		<div style="clear:both;"></div>
 	</div>
-
 </footer>
 <p class="copyright text-center">Copyright 2015 Periodico Que hay. Derechos Reservados</p>
 <?php wp_footer(); ?>
